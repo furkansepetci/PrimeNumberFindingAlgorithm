535736005a99b34513e13da3fa47eadb6de374f1
furkansepetci <furkansepetci@icloud.com>
2022-08-25 03:58:52 +0300

    We simply find the prime numbers up to the given number n.

---
+++ Prime Number Finding Algorithm.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+func findPrimeNumber (border: Int ) {
+    
+    var PrimeNumberArray = [2]
+    var counter = 0
+    var counterTwo = 0
+    let myArray : [Int] = Array(2...border)
+    var divisorArray = [Int]()
+    
+    firstFor : for number in myArray {
+        counter = 0
+        divisorArray = Array(1...number)
+        secondFor : for divisor in divisorArray {
+            if number%divisor == 0{
+                counter+=1
+            }
+            counterTwo+=1 // Indicates how many times the for loop is run.
+        }
+        if counter<3 {
+            PrimeNumberArray.append(number)
+        }
+    }
+    print(PrimeNumberArray)
+    print(counterTwo)
+}
+
+findPrimeNumber(border: 45)
